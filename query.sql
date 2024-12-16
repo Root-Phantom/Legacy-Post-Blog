f3b904827c81d3d97a44a0cbcc3e0cf12742b06a
Nima <144952345+Root-Phantom@users.noreply.github.com>
2024-12-16 21:15:11 +0330

    Update query.sql

--- query.sql
+++ query.sql
@@ -15,3 +15,6 @@ create table posts
 -- sudo nano /etc/mysql/mysql.conf.d/mysqld.cnf
 -- sudo apt install php-pdo php-mysql
 -- sudo systemctl restart mysql
+-- ALTER USER 'root'@'localhost' IDENTIFIED BY '5819';
+-- ALTER USER 'root'@'%' IDENTIFIED BY '5819';
+-- FLUSH PRIVILEGES;
